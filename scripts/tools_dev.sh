1072f45cf3738ce2a5e748d9db005b530b0ca834
Nico Braun <rainbowstack@gmail.com>
2023-07-01 12:52:12 +0200

    fix: set npm config with sudo
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>

--- scripts/tools_dev.sh
+++ scripts/tools_dev.sh
@@ -13,5 +13,5 @@ fi
 if test -z "$(which node)"; then
   curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
   sudo apt-get install -y nodejs
-  npm -g set prefix ~/.local
+  sudo npm -g set prefix ~/.local
 fi
