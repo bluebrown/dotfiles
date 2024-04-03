fbc72c138a95b9f3152473c6ecbf488377736b32
Nico Braun <rainbowstack@gmail.com>
2024-04-04 00:35:57 +0200

    refactor(nvim): kube plugin
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>

--- fsys/home/.config/nvim/lua/blue/k8s.lua
+++ fsys/home/.config/nvim/lua/blue/k8s.lua
@@ -1,27 +1,48 @@
 local M = {}
 
-M.CTX_LIST = "kubectl config get-contexts -o=name"
-M.CTX_USE = "kubectl config use-context"
-M.NS_LIST = "kubectl get ns --no-headers -o=custom-columns=NAME:.metadata.name"
-M.NS_USE = "kubectl config set-context --current --namespace"
+local function fmt(current)
+	return function(item) return item == current and string.format("%s (current)", item) or item end
+end
 
-local make_action = function(list_cmd, use_cmd, title)
-	return function()
-		vim.notify("Loading...")
-		vim.ui.select(vim.fn.systemlist(list_cmd), { prompt = title }, function(item)
-			if not item or item == "" then
-				return
-			end
-			vim.notify(vim.fn.system(use_cmd .. " " .. item))
-		end)
+local function on_select(switch_cmd)
+	return function(item)
+		if not item or item == "" then return end
+		local msg = vim.fn.system(string.format("%s %s", switch_cmd, item))
+		vim.notify(vim.trim(msg))
 	end
 end
 
+function to_choice(cmd)
+	return function()
+		vim.ui.select(
+			vim.fn.systemlist(cmd.list),
+			{ format_item = fmt(vim.fn.system(cmd.current)), prompt = cmd.prompt },
+			on_select(cmd.switch)
+		)
+	end
+end
+
+local cmd_context = {
+	prompt = "Select context",
+	current = "kubectl config view --minify -o jsonpath='{.contexts[].name}'",
+	list = "kubectl config get-contexts -o=name",
+	switch = "kubectl config use-context",
+}
+
+local cmd_namespace = {
+	prompt = "Select namespace",
+	current = "kubectl config view --minify -o jsonpath='{..namespace}'",
+	list = "kubectl get ns --no-headers -o=custom-columns=NAME:.metadata.name",
+	switch = "kubectl config set-context --current --namespace",
+}
+
+M.switch_context = to_choice(cmd_context)
+M.switch_namespace = to_choice(cmd_namespace)
+
 M.setup = function()
 	pcall(require("which-key").register, { ["<leader>k"] = { name = "(k)ubernetes" } })
-
-	vim.keymap.set("n", "<leader>kc", make_action(M.CTX_LIST, M.CTX_USE, "Select Context"), { desc = "(c)ontext" })
-	vim.keymap.set("n", "<leader>kn", make_action(M.NS_LIST, M.NS_USE, "Select Namespace"), { desc = "(n)amespace" })
+	vim.keymap.set("n", "<leader>kc", M.switch_context, { desc = "(c)ontext" })
+	vim.keymap.set("n", "<leader>kn", M.switch_namespace, { desc = "(n)amespace" })
 end
 
 return M
