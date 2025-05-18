e09525b67e961b58a0d3964387e22b7352cd6e8a
Nico Braun <rainbowstack@gmail.com>
2025-05-18 03:39:03 +0200

    skip clone if repo already exist
    Signed-off-by: Nico Braun <rainbowstack@gmail.com>

--- .config/bootstrap.sh
+++ .config/bootstrap.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -Eeo pipefail
 
-git clone --bare https://github.com/bluebrown/dotfiles.git ~/.config/dotfiles.git
-git --git-dir="$HOME/.config/dotfiles.git/" --work-tree="$HOME" checkout
+if ! [[ -d "$HOME/.config/dotfiles.git" ]]; then
+	git clone --bare https://github.com/bluebrown/dotfiles.git ~/.config/dotfiles.git
+	git --git-dir="$HOME/.config/dotfiles.git/" --work-tree="$HOME" checkout
+fi
+
 sudo bash ~/.config/apt_deps.sh
 bash ~/.config/tools.sh
--- .config/tools.sh
+++ .config/tools.sh
@@ -5,20 +5,20 @@ PREFIX="$HOME/.local"
 BIN_DIR="$PREFIX/bin"
 mkdir -p "$BIN_DIR"
 
-# starship
+echo "install starship"
 STARSHIP_URL="https://starship.rs/install.sh"
 
 curl -fsSL "$STARSHIP_URL" | sh -s -- -y -b "$BIN_DIR"
 
-# lsd
+echo "install lsd"
 LSD_VERSION="1.1.5"
 LSD_RELEASE="lsd-v${LSD_VERSION}-x86_64-unknown-linux-gnu"
 LSD_URL="https://github.com/lsd-rs/lsd/releases/download/v${LSD_VERSION}/${LSD_RELEASE}.tar.gz"
 LSD_BIN="${LSD_RELEASE}/lsd"
 
-curl -fSSL "$LSD_URL" | tar -C "$BIN_DIR" --strip-components 1 -xzf - "$LSD_BIN"
+curl -fsSL "$LSD_URL" | tar -C "$BIN_DIR" --strip-components 1 -xzf - "$LSD_BIN"
 
-# bat
+echo "install bat"
 BAT_VERSION="0.25.0"
 BAT_RELEASE="bat-v${BAT_VERSION}-x86_64-unknown-linux-gnu"
 BAT_URL="https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/${BAT_RELEASE}.tar.gz"
@@ -26,7 +26,7 @@ BAT_BIN="${BAT_RELEASE}/bat"
 
 curl -fsSL "$BAT_URL" | tar -C "$BIN_DIR" --strip-components 1 -xzf - "$BAT_BIN"
 
-# fzf
+echo "install fzf"
 FZF_VERSION="0.62.0"
 FZF_RELEASE="fzf-${FZF_VERSION}-linux_amd64"
 FZF_URL="https://github.com/junegunn/fzf/releases/download/v${FZF_VERSION}/${FZF_RELEASE}.tar.gz"
@@ -34,23 +34,23 @@ FZF_BIN="fzf"
 
 curl -fsSL "$FZF_URL" | tar -C "$BIN_DIR" -xzf - "$FZF_BIN"
 
-# ripgrep
+echo "install ripgrep"
 RIPGREP_VERSION="14.1.1"
-RIPGREP_RELEASE="ripgrep-${RIPGREP_VERSION}-x86_64-unknown-linux-gnu"
+RIPGREP_RELEASE="ripgrep-${RIPGREP_VERSION}-x86_64-unknown-linux-musl"
 RIPGREP_URL="https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/${RIPGREP_RELEASE}.tar.gz"
 RIPGREP_BIN="${RIPGREP_RELEASE}/rg"
 
 curl -fsSL "$RIPGREP_URL" | tar -C "$BIN_DIR" --strip-components 1 -xzf - "$RIPGREP_BIN"
 
-# fd
+echo "install fd"
 FD_VERSION="10.2.0"
 FD_RELEASE="fd-v${FD_VERSION}-x86_64-unknown-linux-gnu"
-FD_URL="https://github.com/sharkdp/fd/releases/download/${FD_VERSION}/${FD_RELEASE}.tar.gz"
+FD_URL="https://github.com/sharkdp/fd/releases/download/v${FD_VERSION}/${FD_RELEASE}.tar.gz"
 FD_BIN="${FD_RELEASE}/fd"
 
 curl -fsSL "$FD_URL" | tar -C "$BIN_DIR" --strip-components 1 -xzf - "$FD_BIN"
 
-# direnv
+echo "install direnv"
 DIRENV_VERSION="2.36.0"
 DIRENV_RELEASE="direnv.linux-amd64"
 DIRENV_URL="https://github.com/direnv/direnv/releases/download/v${DIRENV_VERSION}/${DIRENV_RELEASE}"
@@ -59,7 +59,7 @@ DIRENV_BIN="${BIN_DIR}/direnv"
 curl -fsSL "$DIRENV_URL" -o "$DIRENV_BIN"
 chmod +x "$DIRENV_BIN"
 
-# neovim
+echo "install neovim"
 NEOVIM_URL="https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
 
 curl -fsSL "$NEOVIM_URL" | tar -C "$PREFIX" --strip-components 1 -xzf -
