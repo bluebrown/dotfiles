b4aeb28aab13dd9b1cfbd89978b836d2fa0c4510
bluebrown <39703898+bluebrown@users.noreply.github.com>
2026-06-07 16:50:55 +0200

    fix missing deps
    Signed-off-by: bluebrown <39703898+bluebrown@users.noreply.github.com>

--- .config/deps.sh
+++ .config/deps.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-apt install -y $(awk -F# '/^[a-z]+/ {print $1}' <"$HOME/.config/apt.ini")
+sudo apt install -y $(awk -F# '/^[a-z]+/ {print $1}' <"$HOME/.config/apt.ini")
--- .config/nvim/tools.sh
+++ .config/nvim/tools.sh
@@ -1,15 +1,26 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
-# must install nvim and go first
-
-sudo apt install cargo shfmt shellcheck python3-neovim
+mkdir -p .local
+cd .local/
 
 cargo install stylua --features luajit
-
-go install github.com/google/yamlfmt/cmd/yamlfmt@latest
-go install golang.org/x/tools/gopls@latest
-go install golang.org/x/tools/cmd/goimports@latest
-
-curl -fsSL https://github.com/tamasfe/taplo/releases/latest/download/taplo-linux-x86_64.gz | gzip -d - | install -m 755 /dev/stdin ~/.local/bin/taplo
-
 cargo install tree-sitter-cli
+
+curl -fsSL https://github.com/tamasfe/taplo/releases/latest/download/taplo-linux-x86_64.gz |
+  gzip -d - | install -m 755 /dev/stdin ~/.local/bin/taplo
+
+rm -rf go
+vers="$(curl -sL 'https://go.dev/dl/?mode=json' | jq -r '.[0].version')"
+curl -LO "https://go.dev/dl/$vers.linux-amd64.tar.gz"
+tar -xzf "$vers.linux-amd64.tar.gz"
+ln -sr go/bin/* bin/ || true
+
+~/.local/bin/go install github.com/google/yamlfmt/cmd/yamlfmt@latest
+~/.local/bin/go install golang.org/x/tools/gopls@latest
+~/.local/bin/go install golang.org/x/tools/cmd/goimports@latest
+
+rm -rf nvim-linux-x86_64
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
+tar -xzf nvim-linux-x86_64.tar.gz
+ln -sr nvim-linux-x86_64/bin/* bin/ || true
