50fd532c8be7c57dc41bf05cefdae18bc122290c
bluebrown <39703898+bluebrown@users.noreply.github.com>
2026-06-13 09:36:02 +0200

    use cache dir
    Signed-off-by: bluebrown <39703898+bluebrown@users.noreply.github.com>

--- recipe/go.sh
+++ recipe/go.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-mkdir -p .local/bin
-cd .local/
-rm -rf go
+cache_home="$HOME/.cache/tools"
+tool_home="$HOME/.local/tools"
+bin_home="$HOME/.local/bin"
+go_home="$tool_home/go"
+
+rm -rf "$go_home"
+mkdir -p "$cache_home" "$bin_home" "$go_home"
 
 vers="$(curl -sL 'https://go.dev/dl/?mode=json' | jq -r '.[0].version')"
 
+cd "$cache_home"
 curl -LO "https://go.dev/dl/$vers.linux-amd64.tar.gz"
-tar -xzf "$vers.linux-amd64.tar.gz"
+tar Cxzf "$HOME/.local/tools" "$vers.linux-amd64.tar.gz"
 
-ln -sr go/bin/* bin/ || true
+ln -rsf "$go_home/bin"/* "$bin_home/"
--- recipe/nvim.sh
+++ recipe/nvim.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env sh
 set -eu
 
-mkdir -p "$HOME/.local/bin"
-cd "$HOME/.local/"
+cache_home="$HOME/.cache/tools"
+tool_home="$HOME/.local/tools"
+bin_home="$HOME/.local/bin"
+nvim_home="$tool_home/nvim-linux-x86_64"
+
+mkdir -p "$cache_home" "$bin_home" "$nvim_home"
 
 cargo install stylua --features luajit
 cargo install tree-sitter-cli
 
-if ! test -x "$HOME/.local/bin/taplo"; then
+if ! test -x "$bin_home/taplo"; then
   curl -fsSL https://github.com/tamasfe/taplo/releases/latest/download/taplo-linux-x86_64.gz |
-    gzip -d - | install -m 755 /dev/stdin "$HOME/.local/bin/taplo"
+    gzip -d - | install -m 755 /dev/stdin "$bin_home/taplo"
 fi
 
 if test -x go; then
@@ -18,10 +22,13 @@ if test -x go; then
   go install golang.org/x/tools/cmd/goimports@latest
 fi
 
-rm -rf nvim-linux-x86_64
+cd "$cache_home"
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
-tar -xzf nvim-linux-x86_64.tar.gz
-ln -srf nvim-linux-x86_64/bin/* bin/
+
+rm -rf "$nvim_home" && mkdir -p "$nvim_home"
+tar Cxzf "$tool_home" "$cache_home/nvim-linux-x86_64.tar.gz"
+
+ln -srf "$nvim_home/bin/nvim" "$bin_home/nvim"
 
 if test -x pipx; then
   pipx install mdformat
